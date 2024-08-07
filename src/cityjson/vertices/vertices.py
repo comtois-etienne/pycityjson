ca39ca92a7ea115c8890f651383f8e3d35fad0fb
comtois-etienne <comtois.etienne@outlook.com>
2024-08-07 16:19:58 -0400

    vertices

--- src/cityjson/vertices/vertices.py
+++ src/cityjson/vertices/vertices.py
@@ -1,17 +1,11 @@
 import numpy as np
 
 
-class CityVertices:
-    def __init__(self, cityjson):
-        self._translate = cityjson.origin()
-        self._scale = cityjson.scale()
-        vertices = cityjson._data['vertices'] if 'vertices' in cityjson._data else []
-        vertices = np.array(vertices)
-        vertices = ( vertices * np.array(self._scale) ) + np.array(self._translate)
-        vertices = np.round(vertices, cityjson.precision())
-        vertices = vertices.tolist()
+class Vertices:
+    def __init__(self, city, vertices=[]):
+        self.city = city
         self._vertices = vertices
-    
+
     def __getitem__(self, index):
         if isinstance(index, int):
             return self._vertices[index]
@@ -35,10 +29,10 @@ class CityVertices:
         if self.exists(x, y, z):
             return self._vertices.index(coord)
         return None
-    
+
     def get_min(self, axis=0):
         return min(self.get_axis(axis))
-    
+
     def get_max(self, axis=0):
         return max(self.get_axis(axis))
 
@@ -47,15 +41,13 @@ class CityVertices:
         if not self.exists(x, y, z):
             self._vertices.append(coord)
         return self.get(x, y, z)
-    
+
     def get_axis(self, axis=0):
         return [coord[axis] for coord in self._vertices]
-    
-    def set_origin(self, x, y, z):
-        self._translate = [x, y, z]
 
     def to_cj(self):
         vertices = np.array(self._vertices)
-        vertices = ( vertices - np.array(self._translate) ) / np.array(self._scale)
+        vertices = ( vertices - np.array(self.city.origin) ) / np.array(self.city.scale)
         vertices = np.round(vertices).astype(int)
         return vertices.tolist()
+
