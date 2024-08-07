376ff6ea68ef53c8f12dbd1e2fcea3e64fb6f708
comtois-etienne <comtois.etienne@outlook.com>
2024-08-07 16:19:26 -0400

    cityobjects

--- src/cityjson/cityobjects/cityobject.py
+++ src/cityjson/cityobjects/cityobject.py
@@ -1,7 +1,6 @@
-from .citygeometry.citygeometry import CityGeometry
 from src.uuid.guid import guid, is_guid
-from src.scripts.attribute import get_attribute
 from src.scripts.attribute import round_attribute as _round
+
 
 FIRST_LEVEL_TYPES = [
     "Bridge",
@@ -51,23 +50,9 @@ SECOND_LEVEL_TYPES = {
 }
 
 
-def parse_cityobject(cityjson, uuid, data):
-    ctobj = CityObject(
-        cityjson = cityjson,
-        type = get_attribute(data, 'type', 'GenericCityObject'),
-        attributes = get_attribute(data, 'attributes', {}),
-        geometry = CityGeometry(get_attribute(data, 'geometry', None), cityjson), #todo
-        children = get_attribute(data, 'children', []), # todo link uuids to objects
-        parent = get_attribute(data, 'parent', None) # todo link uuid to object
-    )
-    ctobj.geo_extent = get_attribute(data, 'geographicalExtent', None)
-    ctobj.set_attribute('uuid', uuid)
-    return ctobj
-
-
 class CityObject:
-    def __init__(self, cityjson, type, attributes={}, geometry=None, children=[], parent=None):
-        self.cityjson = cityjson
+    def __init__(self, city, type, attributes={}, geometry=[], children=[], parent=None):
+        self.city = city
         self._uuid = attributes['uuid'] if 'uuid' in attributes else guid()
         self.type = type #todo verif with types
         self.geo_extent = None
@@ -88,7 +73,7 @@ class CityObject:
         if self.attributes != {}:
             cj['attributes'] = self.attributes
         if self.geometry is not None:
-            cj['geometry'] = self.geometry.to_cj()
+            cj['geometry'] = [g.to_cj() for g in self.geometry]
         if self.children != []:
             cj['children'] = [child.uuid for child in self.children]
         if self.parent is not None:
@@ -101,7 +86,7 @@ class CityObject:
     def add_child(self, child):
         self.children.append(child)
         child.set_parent(self)
-        self.cityjson.add_cityobject(child)
+        self.city.add_cityobject(child)
 
     def set_attribute(self, key, value):
         self.attributes[key] = value
@@ -123,11 +108,11 @@ class CityObject:
         return self.geometry
 
     def get_vertices(self, flat=False):
-        return self.geometry.get_vertices(flat)
+        return [g.get_vertices(flat) for g in self.geometry]
 
     def set_geographical_extent(self, overwrite=False):
         if self.geo_extent is None or overwrite:
-            g = self.get_geometry()
+            g = self.get_geometry()[0] #todo multiple geometries
             self.geo_extent = [
                 g.get_min(0), g.get_min(1), g.get_min(2),
                 g.get_max(0), g.get_max(1), g.get_max(2)
@@ -144,4 +129,26 @@ class CityObject:
         return self._uuid
 
 
+class CityGroup(CityObject):
+    def __init__(self, city, attributes={}, geometry=[], children=[], parent=None, children_roles=[]):
+        super().__init__(
+            city, 
+            'CityObjectGroup', 
+            attributes, 
+            geometry, 
+            children, 
+            parent
+        )
+        self.children_roles = children_roles
+
+    def add_child(self, child, role=None):
+        super().add_child(child)
+        if role is not None:
+            self.children_roles.append(role)
+
+    def to_cj(self):
+        cj = super().to_cj()
+        if self.children_roles != [] and len(self.children_roles) == len(self.children):
+            cj['childrenRoles'] = self.children_roles
+        return cj
 
