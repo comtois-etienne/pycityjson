45d2f393058457f6db457efd0cc0d19ce51edb0e
comtois-etienne <comtois.etienne@outlook.com>
2024-08-07 16:22:58 -0400

    Create __init__.py

---
+++ src/cityjson/cityobjects/__init__.py
@@ -0,0 +1,12 @@
+from .cityobject import CityObject, CityGroup
+from .cityobjects import CityObjects
+from .parser import CityObjectsParser
+
+
+__all__ = [
+    'CityObject',
+    'CityGroup',
+    'CityObjects',
+    'CityObjectsParser',
+]
+
