b2ba9ed017c66b26cdd36be99e22f7223533698c
comtois-etienne <comtois.etienne@outlook.com>
2024-08-07 23:51:45 -0400

    sequence correction

--- src/cityjson/parser.py
+++ src/cityjson/parser.py
@@ -17,6 +17,7 @@ class CityParser:
     def read_json(self):
         with open(self.file_path, 'r') as file:
             self.cityjson = json.load(file)
+        return self.cityjson
 
     def write_json(self, city : City, formatted=False):
         cityjson = city.to_cj()
@@ -24,20 +25,20 @@ class CityParser:
 
     # data contains cityjson
     def parse(self, data) -> City:
-        if self.cityjson is None:
-            self.read_json()
-
         city = City()
-        v_parser = VerticesParser(city)
-        co_parser = CityObjectsParser(city)
 
         city.type = get_attribute(data, 'type', 'CityJSON')
         city.version = get_attribute(data, 'version', '2.0')
         city.metadata = get_attribute(data, 'metadata', {})
         city.scale = get_attribute(data, 'transform', 'scale', [0.001, 0.001, 0.001])
         city.origin = get_attribute(data, 'transform', 'translate', [0, 0, 0])
+
+        v_parser = VerticesParser(city)
         city._vertices = v_parser.parse(get_attribute(data, 'vertices', []))
+
+        co_parser = CityObjectsParser(city)
         city._cityobjects = co_parser.parse(get_attribute(data, 'CityObjects', []))
+        
         # city.geometry_template = # todo
 
         return city
