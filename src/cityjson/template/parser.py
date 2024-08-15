c71d64609900046e36ccfb2154d11c2620925001
comtois-etienne <comtois.etienne@outlook.com>
2024-08-14 21:52:25 -0400

    Update parser.py

--- src/cityjson/template/parser.py
+++ src/cityjson/template/parser.py
@@ -1,7 +1,7 @@
 from src.cityjson import City
 from src.cityjson.vertices import VerticesParser
 from src.cityjson.geometry import GeometryParser
-from src.scripts.attribute import get_attribute as get_att
+from src.scripts.attribute import get_attribute
 from .template import GeometryTemplate
 
 
@@ -15,10 +15,10 @@ class GeometryTemplateParser:
         city.scale = [1.0, 1.0, 1.0]
 
         v_parser = VerticesParser(city)
-        city._vertices = v_parser.parse(get_att(data, 'vertices-templates', default=[]))
+        city._vertices = v_parser.parse(get_attribute(data, 'vertices-templates', default=[]))
 
         gm_parser = GeometryParser(city)
-        templates_data = get_att(data, 'templates', default=[])
+        templates_data = get_attribute(data, 'templates', default=[])
         templates = [gm_parser.parse(template) for template in templates_data]
 
         return GeometryTemplate(self.city, templates, city._vertices)
