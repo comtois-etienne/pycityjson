1f973d5d701e468eb2a103c82cd25400a989ec77
comtois-etienne <comtois.etienne@outlook.com>
2024-08-16 16:02:11 -0400

    Update matrix.py

--- src/cityjson/geometry/matrix.py
+++ src/cityjson/geometry/matrix.py
@@ -1,5 +1,4 @@
 import numpy as np
-import math
 
 
 def identity_matrix():
@@ -10,38 +9,54 @@ def identity_matrix():
         0, 0, 0, 1
     ]
 
-def rotate_x(angle):
+
+def rotate_x(angle_degree):
+    angle = np.radians(angle_degree)
     matrix = identity_matrix()
-    matrix[5] = math.cos(angle)
-    matrix[6] = math.sin(angle)
-    matrix[9] = -math.sin(angle)
-    matrix[10] = math.cos(angle)
-    return matrix
+    matrix[5] = np.cos(angle)
+    matrix[6] = np.sin(angle)
+    matrix[9] = -np.sin(angle)
+    matrix[10] = np.cos(angle)
+    return list_to_numpy(matrix)
 
-def rotate_y(angle):
+
+def rotate_y(angle_degree):
+    angle = np.radians(angle_degree)
     matrix = identity_matrix()
-    matrix[0] = math.cos(angle)
-    matrix[2] = -math.sin(angle)
-    matrix[8] = math.sin(angle)
-    matrix[10] = math.cos(angle)
-    return matrix
+    matrix[0] = np.cos(angle)
+    matrix[2] = -np.sin(angle)
+    matrix[8] = np.sin(angle)
+    matrix[10] = np.cos(angle)
+    return list_to_numpy(matrix)
 
-def rotate_z(angle):
+
+def rotate_z(angle_degree):
+    angle = np.radians(angle_degree)
     matrix = identity_matrix()
-    matrix[0] = math.cos(angle)
-    matrix[1] = math.sin(angle)
-    matrix[4] = -math.sin(angle)
-    matrix[5] = math.cos(angle)
-    return matrix
+    matrix[0] = np.cos(angle)
+    matrix[1] = np.sin(angle)
+    matrix[4] = -np.sin(angle)
+    matrix[5] = np.cos(angle)
+    return list_to_numpy(matrix)
 
 
-def round_matrix(matrix):
-    return [round(val, 16) for val in matrix]
+def round_matrix(matrix_list):
+    return [round(val, 16) for val in matrix_list]
+
+
+def list_to_numpy(matrix_list):
+    return np.array(matrix_list).reshape(4, 4)
+
+
+def numpy_to_list(matrix_numpy):
+    return matrix_numpy.flatten().tolist()
 
 
 class TransformationMatrix:
+
+    # matrix is a 4x4 matrix of shape 1x16
     def __init__(self, matrix=None):
-        self.matrix = identity_matrix() if matrix is None else round_matrix(matrix)
+        self.matrix: list = identity_matrix() if matrix is None else round_matrix(matrix)
 
     def __str__(self):
         return str(self.matrix)
@@ -50,9 +65,39 @@ class TransformationMatrix:
         return self.matrix
 
     def get_np_matrix(self):
-        return np.array(self.matrix).reshape(4, 4)
+        return list_to_numpy(self.matrix)
 
     def dot(self, matrix) -> 'TransformationMatrix':
-        new_matrix = np.dot(self.get_np_matrix(), matrix.get_np_matrix()).flatten().tolist()
-        return TransformationMatrix(round_matrix(new_matrix))
+        new_matrix = np.dot(self.get_np_matrix(), matrix.get_np_matrix())
+        new_matrix = round_matrix(numpy_to_list(new_matrix))
+        return TransformationMatrix(new_matrix)
+
+    def move(self, x, y, z) -> 'TransformationMatrix':
+        new_matrix = self.get_np_matrix()
+        new_matrix[0][3] += x
+        new_matrix[1][3] += y
+        new_matrix[2][3] += z
+        return TransformationMatrix(numpy_to_list(new_matrix))
+
+    def scale(self, x, y, z) -> 'TransformationMatrix':
+        new_matrix = self.get_np_matrix()
+        new_matrix[0][0] *= x
+        new_matrix[1][1] *= y
+        new_matrix[2][2] *= z
+        return TransformationMatrix(numpy_to_list(new_matrix))
+    
+    def rotate_x(self, angle_degree) -> 'TransformationMatrix':
+        new_matrix = np.dot(self.get_np_matrix(), rotate_x(angle_degree))
+        return TransformationMatrix(numpy_to_list(new_matrix))
+    
+    def rotate_y(self, angle_degree) -> 'TransformationMatrix':
+        new_matrix = np.dot(self.get_np_matrix(), rotate_y(angle_degree))
+        return TransformationMatrix(numpy_to_list(new_matrix))
+    
+    def rotate_z(self, angle_degree) -> 'TransformationMatrix':
+        new_matrix = np.dot(self.get_np_matrix(), rotate_z(angle_degree))
+        return TransformationMatrix(numpy_to_list(new_matrix))
+
+    def to_cj(self):
+        return self.matrix
 
