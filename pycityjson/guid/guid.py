d041021e3bbc9de12b5bade52239e43f27fbc9ec
comtois-etienne <comtois.etienne@outlook.com>
2024-09-25 21:59:27 -0400

    typing and comments

--- pycityjson/guid/guid.py
+++ pycityjson/guid/guid.py
@@ -12,7 +12,12 @@ from functools import reduce
 chars = string.digits + string.ascii_uppercase + string.ascii_lowercase + '_$'
 
 
-def compress(g):
+def compress(g: str) -> str:
+    """
+    Compress a UUID to an IFC guid
+    :param g: the UUID to compress
+    :return: the compressed IFC guid
+    """
     if isinstance(g, str):
         g = uuid.UUID(g)
     g = g.hex
@@ -24,7 +29,11 @@ def compress(g):
     return ''.join([b64(bs[0], 2)] + [b64((bs[i] << 16) + (bs[i + 1] << 8) + bs[i + 2]) for i in range(1, 16, 3)])
 
 
-def expand(g):
+def expand(g: str) -> str:
+    """
+    Decompress an IFC guid to a UUID
+    :param g: the IFC guid to decompress
+    """
     def b64(v):
         return reduce(lambda a, b: a * 64 + b, map(lambda c: chars.index(c), v))
 
@@ -35,15 +44,28 @@ def expand(g):
     return ''.join(['%02x' % b for b in bs])
 
 
-def split(g):
+def split(g: str) -> str:
+    """
+    Splits a UUID into 5 groups with hyphens
+    :param g: the UUID to split
+    :return: the UUID split in normal format
+    """
     return '%s-%s-%s-%s-%s' % (g[:8], g[8:12], g[12:16], g[16:20], g[20:])
 
 
-def guid():
+def guid() -> str:
+    """
+    Generate a new IFC guid (compressed UUID)
+    """
     return compress(uuid.uuid4())
 
 
-def is_guid(g):
+def is_guid(g: str) -> bool:
+    """
+    Check if a string is a valid IFC guid (compressed UUID) or UUID
+    :param g: the string to check
+    :return: True if the string is a valid IFC guid or UUID, False otherwise
+    """
     try:
         g = str(g)
         val = expand(g) if (len(g) == 22 and ('-' not in g)) else g
